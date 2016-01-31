2a6a3a31faece0c8c98606dcbffa671cde97550a
Camilo Ortegon <cjortegon@gmail.com>
2016-01-31 16:11:11 -0500

    Adding ColorConverter empty class.

---
+++ TestProject/src/co/edu/icesi/frutificator/util/ColorConverter.java
@@ -0,0 +1,13 @@
+package co.edu.icesi.frutificator.util;
+
+public class ColorConverter {
+	
+	public static double[] rgb2xyz(double[] rgb) {
+		return null;
+	}
+	
+	public static double[] xyz2lab(double[] xyz) {
+		return null;
+	}
+
+}
