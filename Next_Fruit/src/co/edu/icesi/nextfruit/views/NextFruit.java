f65fb54b4d0af742b6926f5cddbf01305e43ecc1
Camilo Ortegon <cjortegon@gmail.com>
2016-02-13 11:49:47 -0500

    Starting the real project

---
+++ Next_Fruit/src/co/edu/icesi/nextfruit/views/NextFruit.java
@@ -0,0 +1,20 @@
+package co.edu.icesi.nextfruit.views;
+
+import java.awt.Dimension;
+
+import visualkey.KFrame;
+
+public class NextFruit extends KFrame {
+
+	private static final Dimension SIZE = new Dimension();
+
+	public NextFruit() {
+		super(SIZE);
+
+
+
+		pack();
+		setVisible(true);
+	}
+
+}
