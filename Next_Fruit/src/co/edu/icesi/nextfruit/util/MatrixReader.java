82636b6c05670ad1b71ee2b1fee7f22e6bf05df5
Juan David Cuero Giraldo <juandc6@gmail.com>
2016-02-14 20:47:53 -0500

    documentation

--- Next_Fruit/src/co/edu/icesi/nextfruit/util/MatrixReader.java
+++ Next_Fruit/src/co/edu/icesi/nextfruit/util/MatrixReader.java
@@ -5,8 +5,19 @@ import java.io.FileReader;
 import java.io.IOException;
 
 
+/**
+ * This static class reads values from a csv file and load a double matrix with those values.
+ * @author JuanD
+ *
+ */
 public class MatrixReader {
 	
+	/**
+	 * Load a common matrix from the csv files.
+	 * @param path, the path of the csv file containing the matrix to use.
+	 * @return Matrix of type double containing the loaded data.
+	 * @throws IOException
+	 */
 	public static double[][] read(String path) throws IOException{
 		double[][] matrixM = new double[3][3];
 		String[] numbers = new String[3];
